e1be0e41ce2d4db99aa8d725aee1411e7e16cc15
MarciaPratas2000 <156352013+MarciaPratas2000@users.noreply.github.com>
2024-06-25 19:55:04 +0100

    feat: Added "View More" button for definitions and styled the button

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,40 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import SynonymsAntonyms from "./SynonymsAntonyms";
 import "./Definitions.css";
 
-
 export default function Meanings(props) {
+  const [showAll, setShowAll] = useState(false);
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div className="meanings">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map((definition, idx) => {
+        if (!showAll && idx >= 3) return null; // Hide definitions beyond the first 3 if not showing all
         return (
           <div key={idx} className="definition">
             <ul>
-            <li>
-              <strong>Definition:</strong> {definition.definition}
-            </li>
-            {definition.example && (
               <li>
-                <p>
-                <em>Example: {definition.example}</em>
-                </p>
+                <strong>Definition:</strong> {definition.definition}
               </li>
-            )}
-            {definition.synonyms.length > 0 && (
-              <li>
-                <strong>Synonyms:</strong> {definition.synonyms.join(", ")}
-              </li>
-            )}
-            {definition.antonyms.length > 0 && (
-              <li>
-                <strong>Antonyms:</strong> {definition.antonyms.join(", ")}
-              </li>
-            )}
+              {definition.example && (
+                <li>
+                  <p>
+                    <em>Example: {definition.example}</em>
+                  </p>
+                </li>
+              )}
+              {definition.synonyms.length > 0 && (
+                <li>
+                  <strong>Synonyms:</strong> {definition.synonyms.join(", ")}
+                </li>
+              )}
+              {definition.antonyms.length > 0 && (
+                <li>
+                  <strong>Antonyms:</strong> {definition.antonyms.join(", ")}
+                </li>
+              )}
             </ul>
           </div>
         );
       })}
+      {!showAll && props.meaning.definitions.length > 3 && (
+        <ul>
+        <button onClick={toggleShowAll} className=" view-more-btn">
+          View More Definitions
+        </button>
+        </ul>
+      )}
+      {showAll && (
+        <ul>
+        <button onClick={toggleShowAll} className="  view-more-btn">
+          View Less Definitions
+        </button>
+        </ul>
+      )}
       <SynonymsAntonyms
         synonyms={props.meaning.synonyms}
         antonyms={props.meaning.antonyms}
@@ -42,4 +62,3 @@ export default function Meanings(props) {
     </div>
   );
 }
-
